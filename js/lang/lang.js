a12004b578f7b59bff819fa821f19d98390d0e32
malkolp <mf64849@gmail.com>
2022-04-15 02:07:24 +0700

    integrate lang

--- js/lang/lang.js
+++ js/lang/lang.js
@@ -3,13 +3,19 @@
 (()=>{
     const app                   = {
         lang                    : undefined,
+        size                    : undefined,
         init                    : ()=>{
             app.lang            = {};
+            app.size            = 0;
         },
         setLang                 : (key, toggle, lang, ins_=app.lang)=>{
             const obj_lang      = {};
 
-            lang[key]           = obj_lang;
+            ins_[key]           = obj_lang;
         },
     };
+
+    app.init();
+    app.setLang('id', '#toggle-lang-id', window.lang_id);
+    app.setLang('en', '#toggle-lang-en', window.lang_en);
 })();
