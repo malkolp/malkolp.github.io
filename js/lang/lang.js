e6a1803df120bf4aacdb46e9ce2d8c01be4e27b4
malkolp <mf64849@gmail.com>
2022-04-15 02:17:52 +0700

    component integration lang

--- js/lang/lang.js
+++ js/lang/lang.js
@@ -1,17 +1,28 @@
-// noinspection JSUnresolvedVariable
+// noinspection JSUnresolvedVariable,JSUnresolvedFunction
 
 (()=>{
     const app                   = {
         lang                    : undefined,
         size                    : undefined,
+        toggles                 : undefined,
         init                    : ()=>{
             app.lang            = {};
             app.size            = 0;
+            app.toggles         = $();
         },
-        setLang                 : (key, toggle, lang, ins_=app.lang)=>{
+        setLang                 : (key, toggle, lang)=>{
             const obj_lang      = {};
+            const new_lang      = [];
 
-            ins_[key]           = obj_lang;
+            lang.forEach(item=>{
+
+            });
+
+            obj_lang['key']     = key;
+            obj_lang['toggle']  = toggle;
+            obj_lang['lang']    = new_lang;
+
+            app.lang[key]       = obj_lang;
         },
     };
 
