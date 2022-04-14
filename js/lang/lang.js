d764389f5ce49a2aaf26018efb0591392426eb26
malkolp <mf64849@gmail.com>
2022-04-15 02:42:38 +0700

    component integration lang

--- js/lang/lang.js
+++ js/lang/lang.js
@@ -15,14 +15,20 @@
             const new_lang      = [];
 
             lang.forEach(item=>{
-
+                new_lang.push({
+                    element     : document.getElementById(item.target),
+                    value       : item.value,
+                });
             });
 
             obj_lang['key']     = key;
-            obj_lang['toggle']  = toggle;
+            obj_lang['toggle']  = $(toggle);
             obj_lang['lang']    = new_lang;
-
             app.lang[key]       = obj_lang;
+
+            if (++app.size > 0) {
+
+            }
         },
     };
 
