fcf9ac3123fb831fec3ed7550d8127311f946952
japanid133 <baguspw2405@gmail.com>
2022-04-15 18:45:12 +0700

    update lang chinese patch

--- js/lang/lang-data.js
+++ js/lang/lang-data.js
@@ -84,11 +84,11 @@ window.lang_id      = [
     },
     {
         target      : 'about-intro-p1',
-        value       : 'Saya Malik Pajar Lapele, seorang teknisi sekaligus pengembang perangkat lunak dengan pengalaman 5+ tahun di perusahaaan bank terbaik di Asia dan beberapa instansi pemerintah. Tersertifikasi Oracle Certified Master (OCM) dan memiliki pengetahuan inti dalam rekayasa perangkat lunak, peningkatan kinerja sistem, pengembangan berbagai jenis perangkat lunak dan berkontribusi dalam banyak proyek dengan tim global',
+        value       : 'Saya Malik Pajar Lapele, seorang teknisi sekaligus pengembang perangkat lunak dengan pengalaman 5+ tahun di perusahaaan bank terbaik di Asia dan beberapa instansi pemerintah. Tersertifikasi Oracle Certified Master (OCM) dan memiliki pengetahuan inti dalam rekayasa perangkat lunak, peningkatan kinerja sistem, pengembangan berbagai jenis perangkat lunak dan berkontribusi dalam banyak proyek dengan tim global.',
     },
     {
         target      : 'about-intro-p2',
-        value       : 'Selain itu, aktif melakukan penelitian di ranah e-bisnis, sistem informasi dan saat ini sedang mengembangkan teknologi baru yang dapat diterapkan ke dalam proses transformasi digital pada bisnis pendidikan',
+        value       : 'Selain itu, aktif melakukan penelitian di ranah e-bisnis, sistem informasi dan saat ini sedang mengembangkan teknologi baru yang dapat diterapkan ke dalam proses transformasi digital pada bisnis pendidikan.',
     },
     {
         target      : 'experience-title',
@@ -97,5 +97,56 @@ window.lang_id      = [
     {
         target      : 'experience-subtitle',
         value       : 'Apa yang saya kuasai.',
+    }
+];
+
+window.lang_cn      = [
+    {
+        target      : 'nav-item-home',
+        value       : '家',
+    },
+    {
+        target      : 'nav-item-about',
+        value       : '关于',
+    },
+    {
+        target      : 'nav-item-experience',
+        value       : '经验',
+    },
+    {
+        target      : 'nav-item-work',
+        value       : '作品',
+    },
+    {
+        target      : 'nav-item-contact',
+        value       : '接触',
+    },
+    {
+        target      : 'subtitle-detail',
+        value       : '软件工程师和软件开发人员',
+    },
+    {
+        target      : 'about-intro-title',
+        value       : '介绍',
+    },
+    {
+        target      : 'about-intro-subtitle',
+        value       : '关于我',
+    },
+    {
+        target      : 'about-intro-p1',
+        value       : '我是 Malik Pajar Lapele，高级软件工程师，在亚洲最好的银行公司和一些政府机构工作 5 年以上。通过 Oracle Certified Master (OCM) 认证，拥有软件工程、提高系统性能、开发各种软件以及与全球团队一起参与无数项目的核心知识。',
+    },
+    {
+        target      : 'about-intro-p2',
+        value       : '此外，积极在电子商务、信息系统领域进行研究，目前正在发明可应用于教育业务数字化转型过程的新技术。',
+    },
+    {
+        target      : 'experience-title',
+        value       : '经验',
+    },
+    {
+        target      : 'experience-subtitle',
+        value       : '我擅长什么。',
     }
 ];
