6ee857dff4d51cf9ca16dc6288c5a0b7bf2ec313
japanid133 <baguspw2405@gmail.com>
2022-06-19 20:21:43 +0700

    update project preview

--- project/assets/add/dist/qr-reader/scanner.js
+++ project/assets/add/dist/qr-reader/scanner.js
@@ -11,23 +11,9 @@ window.scanner_     = ((id='camera-input', fps=30)=>{
     let cam;
 
     navigator.getUserMedia({video: true}, () => {
-        if (!DetectRTC.isWebsiteHasWebcamPermissions) {
-            errors.permission.removeClass('d-none');
-            errors.null.addClass('d-none');
-        }
-        else
-            errors.error.addClass('d-none');
-
-        Html5Qrcode.getCameras().then(()=>{
-            errors.error.addClass('d-none');
-            $('.camera-rtc').removeClass('d-none');
-
-        });
-
-        cam = new Html5QrcodeScanner(id, {fps: fps, qrbox: { width: 250, height: 250 },});
-        cam.render(()=>{});
-
-        cam.start({ facingMode: { exact: "environment"} });
+        errors.error.addClass('d-none');
+        $('.camera-rtc').removeClass('d-none');
+        cam = new QrScanner(document.getElementById(id), result => console.log('decoded qr code:', result));
     }, () => {
         errors.permission.addClass('d-none');
         errors.null.removeClass('d-none');
